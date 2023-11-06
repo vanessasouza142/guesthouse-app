e115407dd40b424ae900f8e321f9dea131ffd30c
vanessasouza142 <vanessa.kbs16@gmail.com>
2023-11-05 23:03:12 -0300

    Testes unitários

--- spec/models/custom_price_spec.rb
+++ spec/models/custom_price_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe Room, type: :model do
+RSpec.describe CustomPrice, type: :model do
   describe '#valid?' do
     context 'presence' do
-      it 'falso quando Nome está vazio' do
+      it 'falso quando Data de início está vazio' do
         #Arrange
         paulo = User.create!(name: 'Paulo Menezes', email: 'paulomenezes@gmail.com', password: 'password', role: 'host')
         g = Guesthouse.create!(corporate_name: 'Pousada Muro Alto Ltda', brand_name: 'Pousada Muro Alto', registration_number:'39165040000129', 
@@ -11,16 +11,17 @@ RSpec.describe Room, type: :model do
                             neighborhood: 'Muro Alto', state: 'Pernambuco', city: 'Ipojuca', postal_code: '54350820', 
                             description: 'Pousada a beira mar maravilhosa', payment_method: 'Dinheiro, pix e cartão', pet_agreement: 'sim',
                             usage_policy: 'Proibido fumar nas áreas de convivência', check_in: '13:00', check_out: '12:00', user: paulo)
-        room = Room.new(name: '', description: 'Quarto amplo com vista para o mar', area: '10', max_guest: '4', default_price: '210,00',
+        room = Room.create!(name: 'Quarto Girassol', description: 'Quarto amplo com vista para o mar', area: '10', max_guest: '4', default_price: '210,00',
                         bathroom: 'sim', balcony: 'sim', air_conditioner: 'sim', tv: 'sim', wardrobe: 'sim', safe: 'não', accessible: 'sim',
                         guesthouse: g)
+        custom_price = CustomPrice.new(begin_date: '', end_date: '31/12/2023', price: '350,00', room: room)
         #Act
 
         #Assert
-        expect(room).not_to be_valid
+        expect(custom_price).not_to be_valid
       end
 
-      it 'falso quando Descrição está vazio' do
+      it 'falso quando Data de fim está vazio' do
         #Arrange
         paulo = User.create!(name: 'Paulo Menezes', email: 'paulomenezes@gmail.com', password: 'password', role: 'host')
         g = Guesthouse.create!(corporate_name: 'Pousada Muro Alto Ltda', brand_name: 'Pousada Muro Alto', registration_number:'39165040000129', 
@@ -28,16 +29,17 @@ RSpec.describe Room, type: :model do
                             neighborhood: 'Muro Alto', state: 'Pernambuco', city: 'Ipojuca', postal_code: '54350820', 
                             description: 'Pousada a beira mar maravilhosa', payment_method: 'Dinheiro, pix e cartão', pet_agreement: 'sim',
                             usage_policy: 'Proibido fumar nas áreas de convivência', check_in: '13:00', check_out: '12:00', user: paulo)
-        room = Room.new(name: 'Quarto Girassol', description: '', area: '10', max_guest: '4', default_price: '210,00',
+        room = Room.create!(name: 'Quarto Girassol', description: 'Quarto amplo com vista para o mar', area: '10', max_guest: '4', default_price: '210,00',
                         bathroom: 'sim', balcony: 'sim', air_conditioner: 'sim', tv: 'sim', wardrobe: 'sim', safe: 'não', accessible: 'sim',
                         guesthouse: g)
+        custom_price = CustomPrice.new(begin_date: '01/12/2023', end_date: '', price: '350,00', room: room)
         #Act
 
         #Assert
-        expect(room).not_to be_valid
+        expect(custom_price).not_to be_valid
       end
 
-      it 'falso quando Área está vazio' do
+      it 'falso quando Preço da diária está vazio' do
         #Arrange
         paulo = User.create!(name: 'Paulo Menezes', email: 'paulomenezes@gmail.com', password: 'password', role: 'host')
         g = Guesthouse.create!(corporate_name: 'Pousada Muro Alto Ltda', brand_name: 'Pousada Muro Alto', registration_number:'39165040000129', 
@@ -45,30 +47,14 @@ RSpec.describe Room, type: :model do
                             neighborhood: 'Muro Alto', state: 'Pernambuco', city: 'Ipojuca', postal_code: '54350820', 
                             description: 'Pousada a beira mar maravilhosa', payment_method: 'Dinheiro, pix e cartão', pet_agreement: 'sim',
                             usage_policy: 'Proibido fumar nas áreas de convivência', check_in: '13:00', check_out: '12:00', user: paulo)
-        room = Room.new(name: 'Quarto Girassol', description: 'Quarto amplo com vista para o mar', area: '', max_guest: '4', default_price: '210,00',
+        room = Room.create!(name: 'Quarto Girassol', description: 'Quarto amplo com vista para o mar', area: '10', max_guest: '4', default_price: '210,00',
                         bathroom: 'sim', balcony: 'sim', air_conditioner: 'sim', tv: 'sim', wardrobe: 'sim', safe: 'não', accessible: 'sim',
                         guesthouse: g)
+        custom_price = CustomPrice.new(begin_date: '01/12/2023', end_date: '31/12/2023', price: '', room: room)
         #Act
 
         #Assert
-        expect(room).not_to be_valid
-      end
-
-      it 'falso quando Quantidade max. de hóspedes está vazio' do
-        #Arrange
-        paulo = User.create!(name: 'Paulo Menezes', email: 'paulomenezes@gmail.com', password: 'password', role: 'host')
-        g = Guesthouse.create!(corporate_name: 'Pousada Muro Alto Ltda', brand_name: 'Pousada Muro Alto', registration_number:'39165040000129', 
-                            phone_number: '8134658799', email: 'pousadamuroalto@gmail.com', address: 'Av. Beira Mar, 45', 
-                            neighborhood: 'Muro Alto', state: 'Pernambuco', city: 'Ipojuca', postal_code: '54350820', 
-                            description: 'Pousada a beira mar maravilhosa', payment_method: 'Dinheiro, pix e cartão', pet_agreement: 'sim',
-                            usage_policy: 'Proibido fumar nas áreas de convivência', check_in: '13:00', check_out: '12:00', user: paulo)
-        room = Room.new(name: 'Quarto Girassol', description: 'Quarto amplo com vista para o mar', area: '10', max_guest: '', default_price: '210,00',
-                        bathroom: 'sim', balcony: 'sim', air_conditioner: 'sim', tv: 'sim', wardrobe: 'sim', safe: 'não', accessible: 'sim',
-                        guesthouse: g)
-        #Act
-
-        #Assert
-        expect(room).not_to be_valid
+        expect(custom_price).not_to be_valid
       end
     end
   end
--- spec/models/guesthouse_spec.rb
+++ spec/models/guesthouse_spec.rb
@@ -1,4 +1,232 @@
 require 'rails_helper'
 
 RSpec.describe Guesthouse, type: :model do
+  describe '#valid?' do
+    context 'presence' do
+      it 'falso quando Razão Social está vazio' do
+        #Arrange
+        paulo = User.create!(name: 'Paulo Menezes', email: 'paulomenezes@gmail.com', password: 'password', role: 'host')
+        g = Guesthouse.new(corporate_name: '', brand_name: 'Pousada Muro Alto', registration_number:'39165040000129', 
+                            phone_number: '8134658799', email: 'pousadamuroalto@gmail.com', address: 'Av. Beira Mar, 45', 
+                            neighborhood: 'Muro Alto', state: 'Pernambuco', city: 'Ipojuca', postal_code: '54350820', 
+                            description: 'Pousada a beira mar maravilhosa', payment_method: 'Dinheiro, pix e cartão', pet_agreement: 'sim',
+                            usage_policy: 'Proibido fumar nas áreas de convivência', check_in: '13:00', check_out: '12:00', user: paulo)
+        #Act
+
+        #Assert
+        expect(g).not_to be_valid
+      end
+
+      it 'falso quando Nome Fantasia está vazio' do
+        #Arrange
+        paulo = User.create!(name: 'Paulo Menezes', email: 'paulomenezes@gmail.com', password: 'password', role: 'host')
+        g = Guesthouse.new(corporate_name: 'Pousada Muro Alto Ltda', brand_name: '', registration_number:'39165040000129', 
+                            phone_number: '8134658799', email: 'pousadamuroalto@gmail.com', address: 'Av. Beira Mar, 45', 
+                            neighborhood: 'Muro Alto', state: 'Pernambuco', city: 'Ipojuca', postal_code: '54350820', 
+                            description: 'Pousada a beira mar maravilhosa', payment_method: 'Dinheiro, pix e cartão', pet_agreement: 'sim',
+                            usage_policy: 'Proibido fumar nas áreas de convivência', check_in: '13:00', check_out: '12:00', user: paulo)
+        #Act
+
+        #Assert
+        expect(g).not_to be_valid
+      end
+
+      it 'falso quando CNPJ está vazio' do
+        #Arrange
+        paulo = User.create!(name: 'Paulo Menezes', email: 'paulomenezes@gmail.com', password: 'password', role: 'host')
+        g = Guesthouse.new(corporate_name: 'Pousada Muro Alto Ltda', brand_name: 'Pousada Muro Alto', registration_number:'', 
+                            phone_number: '8134658799', email: 'pousadamuroalto@gmail.com', address: 'Av. Beira Mar, 45', 
+                            neighborhood: 'Muro Alto', state: 'Pernambuco', city: 'Ipojuca', postal_code: '54350820', 
+                            description: 'Pousada a beira mar maravilhosa', payment_method: 'Dinheiro, pix e cartão', pet_agreement: 'sim',
+                            usage_policy: 'Proibido fumar nas áreas de convivência', check_in: '13:00', check_out: '12:00', user: paulo)
+        #Act
+
+        #Assert
+        expect(g).not_to be_valid
+      end
+
+      it 'falso quando Telefone para contato está vazio' do
+        #Arrange
+        paulo = User.create!(name: 'Paulo Menezes', email: 'paulomenezes@gmail.com', password: 'password', role: 'host')
+        g = Guesthouse.new(corporate_name: 'Pousada Muro Alto Ltda', brand_name: 'Pousada Muro Alto', registration_number:'39165040000129', 
+                            phone_number: '', email: 'pousadamuroalto@gmail.com', address: 'Av. Beira Mar, 45', 
+                            neighborhood: 'Muro Alto', state: 'Pernambuco', city: 'Ipojuca', postal_code: '54350820', 
+                            description: 'Pousada a beira mar maravilhosa', payment_method: 'Dinheiro, pix e cartão', pet_agreement: 'sim',
+                            usage_policy: 'Proibido fumar nas áreas de convivência', check_in: '13:00', check_out: '12:00', user: paulo)
+        #Act
+
+        #Assert
+        expect(g).not_to be_valid
+      end
+
+      it 'falso quando E-mail para contato está vazio' do
+        #Arrange
+        paulo = User.create!(name: 'Paulo Menezes', email: 'paulomenezes@gmail.com', password: 'password', role: 'host')
+        g = Guesthouse.new(corporate_name: 'Pousada Muro Alto Ltda', brand_name: 'Pousada Muro Alto', registration_number:'39165040000129', 
+                            phone_number: '8134658799', email: '', address: 'Av. Beira Mar, 45', 
+                            neighborhood: 'Muro Alto', state: 'Pernambuco', city: 'Ipojuca', postal_code: '54350820', 
+                            description: 'Pousada a beira mar maravilhosa', payment_method: 'Dinheiro, pix e cartão', pet_agreement: 'sim',
+                            usage_policy: 'Proibido fumar nas áreas de convivência', check_in: '13:00', check_out: '12:00', user: paulo)
+        #Act
+
+        #Assert
+        expect(g).not_to be_valid
+      end
+
+      it 'falso quando Endereço está vazio' do
+        #Arrange
+        paulo = User.create!(name: 'Paulo Menezes', email: 'paulomenezes@gmail.com', password: 'password', role: 'host')
+        g = Guesthouse.new(corporate_name: 'Pousada Muro Alto Ltda', brand_name: 'Pousada Muro Alto', registration_number:'39165040000129', 
+                            phone_number: '8134658799', email: 'pousadamuroalto@gmail.com', address: '', 
+                            neighborhood: 'Muro Alto', state: 'Pernambuco', city: 'Ipojuca', postal_code: '54350820', 
+                            description: 'Pousada a beira mar maravilhosa', payment_method: 'Dinheiro, pix e cartão', pet_agreement: 'sim',
+                            usage_policy: 'Proibido fumar nas áreas de convivência', check_in: '13:00', check_out: '12:00', user: paulo)
+        #Act
+
+        #Assert
+        expect(g).not_to be_valid
+      end
+
+      it 'falso quando Bairro está vazio' do
+        #Arrange
+        paulo = User.create!(name: 'Paulo Menezes', email: 'paulomenezes@gmail.com', password: 'password', role: 'host')
+        g = Guesthouse.new(corporate_name: 'Pousada Muro Alto Ltda', brand_name: 'Pousada Muro Alto', registration_number:'39165040000129', 
+                            phone_number: '8134658799', email: 'pousadamuroalto@gmail.com', address: 'Av. Beira Mar, 45', 
+                            neighborhood: '', state: 'Pernambuco', city: 'Ipojuca', postal_code: '54350820', 
+                            description: 'Pousada a beira mar maravilhosa', payment_method: 'Dinheiro, pix e cartão', pet_agreement: 'sim',
+                            usage_policy: 'Proibido fumar nas áreas de convivência', check_in: '13:00', check_out: '12:00', user: paulo)
+        #Act
+
+        #Assert
+        expect(g).not_to be_valid
+      end
+
+      it 'falso quando Estado está vazio' do
+        #Arrange
+        paulo = User.create!(name: 'Paulo Menezes', email: 'paulomenezes@gmail.com', password: 'password', role: 'host')
+        g = Guesthouse.new(corporate_name: 'Pousada Muro Alto Ltda', brand_name: 'Pousada Muro Alto', registration_number:'39165040000129', 
+                            phone_number: '8134658799', email: 'pousadamuroalto@gmail.com', address: 'Av. Beira Mar, 45', 
+                            neighborhood: 'Muro Alto', state: '', city: 'Ipojuca', postal_code: '54350820', 
+                            description: 'Pousada a beira mar maravilhosa', payment_method: 'Dinheiro, pix e cartão', pet_agreement: 'sim',
+                            usage_policy: 'Proibido fumar nas áreas de convivência', check_in: '13:00', check_out: '12:00', user: paulo)
+        #Act
+
+        #Assert
+        expect(g).not_to be_valid
+      end
+
+      it 'falso quando Cidade está vazio' do
+        #Arrange
+        paulo = User.create!(name: 'Paulo Menezes', email: 'paulomenezes@gmail.com', password: 'password', role: 'host')
+        g = Guesthouse.new(corporate_name: 'Pousada Muro Alto Ltda', brand_name: 'Pousada Muro Alto', registration_number:'39165040000129', 
+                            phone_number: '8134658799', email: 'pousadamuroalto@gmail.com', address: 'Av. Beira Mar, 45', 
+                            neighborhood: 'Muro Alto', state: 'Pernambuco', city: '', postal_code: '54350820', 
+                            description: 'Pousada a beira mar maravilhosa', payment_method: 'Dinheiro, pix e cartão', pet_agreement: 'sim',
+                            usage_policy: 'Proibido fumar nas áreas de convivência', check_in: '13:00', check_out: '12:00', user: paulo)
+        #Act
+
+        #Assert
+        expect(g).not_to be_valid
+      end
+
+      it 'falso quando CEP está vazio' do
+        #Arrange
+        paulo = User.create!(name: 'Paulo Menezes', email: 'paulomenezes@gmail.com', password: 'password', role: 'host')
+        g = Guesthouse.new(corporate_name: 'Pousada Muro Alto Ltda', brand_name: 'Pousada Muro Alto', registration_number:'39165040000129', 
+                            phone_number: '8134658799', email: 'pousadamuroalto@gmail.com', address: 'Av. Beira Mar, 45', 
+                            neighborhood: 'Muro Alto', state: 'Pernambuco', city: 'Ipojuca', postal_code: '', 
+                            description: 'Pousada a beira mar maravilhosa', payment_method: 'Dinheiro, pix e cartão', pet_agreement: 'sim',
+                            usage_policy: 'Proibido fumar nas áreas de convivência', check_in: '13:00', check_out: '12:00', user: paulo)
+        #Act
+
+        #Assert
+        expect(g).not_to be_valid
+      end
+
+      it 'falso quando Descrição está vazio' do
+        #Arrange
+        paulo = User.create!(name: 'Paulo Menezes', email: 'paulomenezes@gmail.com', password: 'password', role: 'host')
+        g = Guesthouse.new(corporate_name: 'Pousada Muro Alto Ltda', brand_name: 'Pousada Muro Alto', registration_number:'39165040000129', 
+                            phone_number: '8134658799', email: 'pousadamuroalto@gmail.com', address: 'Av. Beira Mar, 45', 
+                            neighborhood: 'Muro Alto', state: 'Pernambuco', city: 'Ipojuca', postal_code: '54350820', 
+                            description: '', payment_method: 'Dinheiro, pix e cartão', pet_agreement: 'sim',
+                            usage_policy: 'Proibido fumar nas áreas de convivência', check_in: '13:00', check_out: '12:00', user: paulo)
+        #Act
+
+        #Assert
+        expect(g).not_to be_valid
+      end
+
+      it 'falso quando Métodos de Pagamento está vazio' do
+        #Arrange
+        paulo = User.create!(name: 'Paulo Menezes', email: 'paulomenezes@gmail.com', password: 'password', role: 'host')
+        g = Guesthouse.new(corporate_name: 'Pousada Muro Alto Ltda', brand_name: 'Pousada Muro Alto', registration_number:'39165040000129', 
+                            phone_number: '8134658799', email: 'pousadamuroalto@gmail.com', address: 'Av. Beira Mar, 45', 
+                            neighborhood: 'Muro Alto', state: 'Pernambuco', city: 'Ipojuca', postal_code: '54350820', 
+                            description: 'Pousada a beira mar maravilhosa', payment_method: '', pet_agreement: 'sim',
+                            usage_policy: 'Proibido fumar nas áreas de convivência', check_in: '13:00', check_out: '12:00', user: paulo)
+        #Act
+
+        #Assert
+        expect(g).not_to be_valid
+      end
+
+      it 'falso quando Politicas de Uso está vazio' do
+        #Arrange
+        paulo = User.create!(name: 'Paulo Menezes', email: 'paulomenezes@gmail.com', password: 'password', role: 'host')
+        g = Guesthouse.new(corporate_name: 'Pousada Muro Alto Ltda', brand_name: 'Pousada Muro Alto', registration_number:'39165040000129', 
+                            phone_number: '8134658799', email: 'pousadamuroalto@gmail.com', address: 'Av. Beira Mar, 45', 
+                            neighborhood: 'Muro Alto', state: 'Pernambuco', city: 'Ipojuca', postal_code: '54350820', 
+                            description: 'Pousada a beira mar maravilhosa', payment_method: 'Dinheiro, pix e cartão', pet_agreement: 'sim',
+                            usage_policy: '', check_in: '13:00', check_out: '12:00', user: paulo)
+        #Act
+
+        #Assert
+        expect(g).not_to be_valid
+      end
+
+      it 'falso quando Check-in está vazio' do
+        #Arrange
+        paulo = User.create!(name: 'Paulo Menezes', email: 'paulomenezes@gmail.com', password: 'password', role: 'host')
+        g = Guesthouse.new(corporate_name: 'Pousada Muro Alto Ltda', brand_name: 'Pousada Muro Alto', registration_number:'39165040000129', 
+                            phone_number: '8134658799', email: 'pousadamuroalto@gmail.com', address: 'Av. Beira Mar, 45', 
+                            neighborhood: 'Muro Alto', state: 'Pernambuco', city: 'Ipojuca', postal_code: '54350820', 
+                            description: 'Pousada a beira mar maravilhosa', payment_method: 'Dinheiro, pix e cartão', pet_agreement: 'sim',
+                            usage_policy: 'Proibido fumar nas áreas de convivência', check_in: '', check_out: '12:00', user: paulo)
+        #Act
+
+        #Assert
+        expect(g).not_to be_valid
+      end
+
+      it 'falso quando Check-out está vazio' do
+        #Arrange
+        paulo = User.create!(name: 'Paulo Menezes', email: 'paulomenezes@gmail.com', password: 'password', role: 'host')
+        g = Guesthouse.new(corporate_name: 'Pousada Muro Alto Ltda', brand_name: 'Pousada Muro Alto', registration_number:'39165040000129', 
+                            phone_number: '8134658799', email: 'pousadamuroalto@gmail.com', address: 'Av. Beira Mar, 45', 
+                            neighborhood: 'Muro Alto', state: 'Pernambuco', city: 'Ipojuca', postal_code: '54350820', 
+                            description: 'Pousada a beira mar maravilhosa', payment_method: 'Dinheiro, pix e cartão', pet_agreement: 'sim',
+                            usage_policy: 'Proibido fumar nas áreas de convivência', check_in: '13:00', check_out: '5s', user: paulo)
+        #Act
+
+        #Assert
+        expect(g).not_to be_valid
+      end
+    end
+  end
+
+  describe '#full_address' do
+  it 'evibe endereço completo' do
+    #Arrange
+    paulo = User.create!(name: 'Paulo Menezes', email: 'paulomenezes@gmail.com', password: 'password', role: 'host')
+    g = Guesthouse.new(address: 'Av. Beira Mar, 45', neighborhood: 'Muro Alto', state: 'Pernambuco', city: 'Ipojuca', 
+                          postal_code: '54350820', user: paulo)
+
+    #Act
+    result = g.full_address
+
+    #Assert
+    expect(result).to eq 'Av. Beira Mar, 45, Muro Alto, Ipojuca - Pernambuco CEP: 54350820'
+  end
+end
 end
