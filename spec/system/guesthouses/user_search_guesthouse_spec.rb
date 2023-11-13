2fd6799590948007f812bb9cbee65fcdda4e4104
vanessasouza142 <vanessa.kbs16@gmail.com>
2023-11-13 11:53:12 -0300

    Melhorando alguns testes

--- spec/system/guesthouses/user_search_guesthouse_spec.rb
+++ spec/system/guesthouses/user_search_guesthouse_spec.rb
@@ -97,23 +97,23 @@ describe 'Usuário faz a busca de uma pousada' do
     expect(page).to have_content 'Nenhum resultado encontrado.'
   end
 
-  it 'e encontra múltiplas pousadas' do
+  it 'e encontra várias pousadas em ordem alfabética' do
     #Arrange
     paulo = User.create!(name: 'Paulo Menezes', email: 'paulomenezes@gmail.com', password: 'password', role: 'host')
     mariana = User.create!(name: 'Mariana Silva', email: 'mariana@gmail.com', password: 'password', role: 'host')
 
-    g1 = Guesthouse.create!(corporate_name: 'Pousada Muro Alto Ltda', brand_name: 'Pousada Muro Alto', registration_number:'39165040000129', 
-                            phone_number: '8134658799', email: 'pousadamuroalto@gmail.com', address: 'Av. Beira Mar, 45', 
-                            neighborhood: 'Muro Alto', state: 'Pernambuco', city: 'Recife', postal_code: '54350820', 
-                            description: 'Pousada a beira mar maravilhosa', payment_method: 'Dinheiro, pix e cartão', pet_agreement: 'sim',
-                            usage_policy: 'Proibido fumar nas áreas de convivência', check_in: '13:00', check_out: '12:00', status: 'active',
-                            user: paulo)
-    g2 = Guesthouse.create!(corporate_name: 'Pousada Sulamericana Ltda', brand_name: 'Pousada Sulamericana', registration_number:'56897040000129', 
+    g1 = Guesthouse.create!(corporate_name: 'Pousada Sulamericana Ltda', brand_name: 'Pousada Sulamericana', registration_number:'56897040000129', 
                             phone_number: '8138975644', email: 'pousadasulamericana@gmail.com', address: 'Av. Juliana Holanda, 498', 
                             neighborhood: 'Boa Vista', state: 'Pernambuco', city: 'Recife', postal_code: '54560500', 
                             description: 'Pousada com ótima localização', payment_method: 'Dinheiro, pix e cartão', pet_agreement: 'não',
                             usage_policy: 'Proibido fumar nas áreas de convivência', check_in: '13:00', check_out: '12:00', status: 'active',
                             user: mariana)
+    g2 = Guesthouse.create!(corporate_name: 'Pousada Muro Alto Ltda', brand_name: 'Pousada Muro Alto', registration_number:'39165040000129', 
+                            phone_number: '8134658799', email: 'pousadamuroalto@gmail.com', address: 'Av. Beira Mar, 45', 
+                            neighborhood: 'Muro Alto', state: 'Pernambuco', city: 'Recife', postal_code: '54350820', 
+                            description: 'Pousada a beira mar maravilhosa', payment_method: 'Dinheiro, pix e cartão', pet_agreement: 'sim',
+                            usage_policy: 'Proibido fumar nas áreas de convivência', check_in: '13:00', check_out: '12:00', status: 'active',
+                            user: paulo)
     g3 = Guesthouse.create!(corporate_name: 'Pousada Aconchego Ltda', brand_name: 'Pousada Aconchego', registration_number:'78964526644545', 
                             phone_number: '8134587756', email: 'pousadaaconchego@gmail.com', address: 'Av. da Ribeira, 75', 
                             neighborhood: 'Boa Viagem', state: 'Pernambuco', city: 'Paulista', postal_code: '52680200', 
@@ -129,8 +129,38 @@ describe 'Usuário faz a busca de uma pousada' do
     #Assert
     expect(page).to have_content 'Resultados da Busca por: Recife'
     expect(page).to have_content '2 pousadas encontradas'
-    expect(page).to have_link "#{g1.brand_name}"
-    expect(page).to have_link "#{g2.brand_name}"
-    expect(page).not_to have_link "#{g3.brand_name}"
+    links = all('ul li a')
+    expect(links[0].text).to eq(g2.brand_name)
+    expect(links[1].text).to eq(g1.brand_name)
+    expect(links).not_to include(g3.brand_name)
+  end
+
+  it 'e ao clicar no link da pousada, vê os detalhes' do
+    #Arrange
+    mariana = User.create!(name: 'Mariana Silva', email: 'mariana@gmail.com', password: 'password', role: 'host')
+    g = Guesthouse.create!(corporate_name: 'Pousada Sulamericana Ltda', brand_name: 'Pousada Sulamericana', registration_number:'56897040000129', 
+                          phone_number: '8138975644', email: 'pousadasulamericana@gmail.com', address: 'Av. Juliana Holanda, 498', 
+                          neighborhood: 'Boa Vista', state: 'Pernambuco', city: 'Recife', postal_code: '54560500', 
+                          description: 'Pousada com ótima localização', payment_method: 'Dinheiro, pix e cartão', pet_agreement: 'não',
+                          usage_policy: 'Proibido fumar nas áreas de convivência', check_in: '13:00', check_out: '12:00', status: 'active',
+                          user: mariana)
+
+    #Act
+    visit root_path
+    fill_in 'Buscar Pousada', with: 'Recife'
+    click_on 'Buscar'
+    click_on 'Pousada Sulamericana'
+
+    #Assert
+    expect(current_path).to eq guesthouse_path(g.id)
+    expect(page).to have_content 'Pousada Sulamericana'
+    expect(page).to have_content 'Telefone para contato: 8138975644'
+    expect(page).to have_content 'E-mail para contato: pousadasulamericana@gmail.com'
+    expect(page).to have_content 'Endereço Completo: Av. Juliana Holanda, 498, Boa Vista, Recife - Pernambuco CEP: 54560500'
+    expect(page).to have_content 'Descrição: Pousada com ótima localização'
+    expect(page).to have_content 'Métodos de Pagamento: Dinheiro, pix e cartão'
+    expect(page).to have_content 'Aceita animais de estimação? não'
+    expect(page).to have_content 'Check-in: 13:00'
+    expect(page).to have_content 'Check-out: 12:00'
   end
 end
--- spec/system/user_view_city_menu_homepage_spec.rb
+++ spec/system/user_view_city_menu_homepage_spec.rb
@@ -37,23 +37,23 @@ describe 'Usuário vê menu de cidades que tem pousadas cadastradas ativas' do
     end
   end
 
-  it 'e ao clicar em uma cidade do menu, encontra as pousadas dessa cidade' do
+  it 'e ao clicar em uma cidade do menu, encontra as pousadas dessa cidade em ordem alfabética' do
     #Arrange
     paulo = User.create!(name: 'Paulo Menezes', email: 'paulomenezes@gmail.com', password: 'password', role: 'host')
     mariana = User.create!(name: 'Mariana Silva', email: 'mariana@gmail.com', password: 'password', role: 'host')
 
-    g1 = Guesthouse.create!(corporate_name: 'Pousada Muro Alto Ltda', brand_name: 'Pousada Muro Alto', registration_number:'39165040000129', 
+    g1 = Guesthouse.create!(corporate_name: 'Pousada Sulamericana Ltda', brand_name: 'Pousada Sulamericana', registration_number:'56897040000129', 
+                            phone_number: '8138975644', email: 'pousadasulamericana@gmail.com', address: 'Av. Juliana Holanda, 498', 
+                            neighborhood: 'Boa Vista', state: 'Pernambuco', city: 'Ipojuca', postal_code: '54560500', 
+                            description: 'Pousada com ótima localização', payment_method: 'Dinheiro, pix e cartão', pet_agreement: 'não',
+                            usage_policy: 'Proibido fumar nas áreas de convivência', check_in: '13:00', check_out: '12:00', status: 'active',
+                            user: mariana)
+    g2 = Guesthouse.create!(corporate_name: 'Pousada Muro Alto Ltda', brand_name: 'Pousada Muro Alto', registration_number:'39165040000129', 
                             phone_number: '8134658799', email: 'pousadamuroalto@gmail.com', address: 'Av. Beira Mar, 45', 
                             neighborhood: 'Muro Alto', state: 'Pernambuco', city: 'Ipojuca', postal_code: '54350820', 
                             description: 'Pousada a beira mar maravilhosa', payment_method: 'Dinheiro, pix e cartão', pet_agreement: 'sim',
                             usage_policy: 'Proibido fumar nas áreas de convivência', check_in: '13:00', check_out: '12:00', status: 'active',
                             user: paulo)
-    g2 = Guesthouse.create!(corporate_name: 'Pousada Sulamericana Ltda', brand_name: 'Pousada Sulamericana', registration_number:'56897040000129', 
-                            phone_number: '8138975644', email: 'pousadasulamericana@gmail.com', address: 'Av. Juliana Holanda, 498', 
-                            neighborhood: 'Boa Vista', state: 'Pernambuco', city: 'Recife', postal_code: '54560500', 
-                            description: 'Pousada com ótima localização', payment_method: 'Dinheiro, pix e cartão', pet_agreement: 'não',
-                            usage_policy: 'Proibido fumar nas áreas de convivência', check_in: '13:00', check_out: '12:00', status: 'active',
-                            user: mariana)
     g3 = Guesthouse.create!(corporate_name: 'Pousada Aconchego Ltda', brand_name: 'Pousada Aconchego', registration_number:'78964526644545', 
                             phone_number: '8134587756', email: 'pousadaaconchego@gmail.com', address: 'Av. da Ribeira, 75', 
                             neighborhood: 'Boa Viagem', state: 'Pernambuco', city: 'Paulista', postal_code: '52680200', 
@@ -71,10 +71,39 @@ describe 'Usuário vê menu de cidades que tem pousadas cadastradas ativas' do
     expect(current_path).to eq guesthouses_by_city_path(g1.city)
     expect(page).not_to have_content 'Não existem pousadas cadastradas.'
     expect(page).to have_content 'Pousadas em Ipojuca:'
-    within('ul li') do
-      expect(page).to have_link "#{g1.brand_name}"
-      expect(page).not_to have_link "#{g2.brand_name}"
-      expect(page).not_to have_link "#{g3.brand_name}"
+    links = all('ul li a')
+    expect(links[0].text).to eq(g2.brand_name)
+    expect(links[1].text).to eq(g1.brand_name)
+    expect(links).not_to include(g3.brand_name)
+  end
+
+  it 'e ao clicar no link da pousada, vê os detalhes' do
+    #Arrange
+    mariana = User.create!(name: 'Mariana Silva', email: 'mariana@gmail.com', password: 'password', role: 'host')
+    g = Guesthouse.create!(corporate_name: 'Pousada Sulamericana Ltda', brand_name: 'Pousada Sulamericana', registration_number:'56897040000129', 
+                          phone_number: '8138975644', email: 'pousadasulamericana@gmail.com', address: 'Av. Juliana Holanda, 498', 
+                          neighborhood: 'Boa Vista', state: 'Pernambuco', city: 'Ipojuca', postal_code: '54560500', 
+                          description: 'Pousada com ótima localização', payment_method: 'Dinheiro, pix e cartão', pet_agreement: 'não',
+                          usage_policy: 'Proibido fumar nas áreas de convivência', check_in: '13:00', check_out: '12:00', status: 'active',
+                          user: mariana)
+
+    #Act
+    visit root_path
+    within('.menu-cities') do
+      click_on 'Ipojuca'
     end
+    click_on 'Pousada Sulamericana'
+
+    #Assert
+    expect(current_path).to eq guesthouse_path(g.id)
+    expect(page).to have_content 'Pousada Sulamericana'
+    expect(page).to have_content 'Telefone para contato: 8138975644'
+    expect(page).to have_content 'E-mail para contato: pousadasulamericana@gmail.com'
+    expect(page).to have_content 'Endereço Completo: Av. Juliana Holanda, 498, Boa Vista, Ipojuca - Pernambuco CEP: 54560500'
+    expect(page).to have_content 'Descrição: Pousada com ótima localização'
+    expect(page).to have_content 'Métodos de Pagamento: Dinheiro, pix e cartão'
+    expect(page).to have_content 'Aceita animais de estimação? não'
+    expect(page).to have_content 'Check-in: 13:00'
+    expect(page).to have_content 'Check-out: 12:00'
   end
 end
