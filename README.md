c6371dcde52cad5106ce27e41876a9bca407e0cd
vanessasouza142 <vanessa.kbs16@gmail.com>
2023-11-25 23:37:25 -0300

    Documentação da API

--- README.md
+++ README.md
@@ -22,3 +22,94 @@ Things you may want to cover:
 * Deployment instructions
 
 * ...
+
+Documentação da API
+(Não é necessário autenticação para acessar os endpoints abaixo)
+
+Endpoint: GET /api/v1/guesthouses
+Permite listar todas as pousadas cadastradas e ativas.
+Exemplo de Resposta de Sucesso (Status 200):
+[
+  {
+    "brand_name": "Pousada Sulamericana",
+    "city": "Recife",
+  },
+  {
+    "brand_name": "Pousada Aconchego",
+    "city": "Paulista",
+  }
+]
+
+Endpoint: GET /api/v1/guesthouses?search='nome da pousada'
+Permite filtrar as pousadas cadastradas e ativas pelo nome, por meio do parâmetro 'search'
+Exemplo de Resposta de Sucesso (Status 200):
+GET /api/v1/guesthouses?search='Sulamericana'
+[
+  {
+    "brand_name": "Pousada Sulamericana",
+    "city": "Recife",
+  }
+]
+
+Endpoint: GET /api/v1/guesthouses/:id
+Permite obter detalhes de uma pousada ativa, por meio do parâmetro 'id' da pousada.
+Exemplo de Resposta de Sucesso (Status 200):
+{
+  "brand_name": "Pousada Muro Alto",
+  "phone_number": "8134658799",
+  "email": "pousadamuroalto@gmail.com",
+  "address": "Av. Beira Mar, 45",
+  "neighborhood": "Muro Alto",
+  "state": "Pernambuco",
+  "city": "Ipojuca",
+  "postal_code": "54350820",
+  "description": "Pousada a beira mar maravilhosa",
+  "payment_method": "Dinheiro, pix e cartão",
+  "pet_agreement": "sim",
+  "usage_policy": "Proibido fumar nas áreas de convivência",
+  "check_in": "13:00",
+  "check_out": "12:00",
+  "average_score": 3.8
+}
+
+Endpoint: GET /api/v1/guesthouses/:id/rooms
+Permite listar os quartos disponíveis de uma pousada ativa, por meio do parâmetro 'id' da pousada.
+Exemplo de Resposta de Sucesso (Status 200):
+[
+  {
+    "name": "Quarto Padrão",
+    "description": "Quarto bem ventilado",
+    "area": 10,
+    "max_guest": 2,
+    "default_price": 180
+  },
+  {
+    "name": "Quarto Premium",
+    "description": "Quarto maravilhoso",
+    "area": 13,
+    "max_guest": 4,
+    "default_price": 250
+  }
+]
+
+Endpoint: POST /api/v1/rooms/:id/bookings/check_availability
+Permite consultar a disponibilidade de um quarto em certo período, para possivelmente criar uma reserva.
+Parâmetros de requisição obrigatórios:
+id: identificador único do quarto.
+check_in_date: data de entrada desejada. Formato: "YYYY-MM-DD".
+check_out_date: data de saída desejada. Formato: "YYYY-MM-DD".
+guests_number: Número de hóspedes para a reserva.
+Exemplo de Requisição:
+{
+  "booking": {
+    "check_in_date": "2023-12-01",
+    "check_out_date": "2023-12-05",
+    "guests_number": 2
+    "room": 1
+  }
+}
+Exemplo de Resposta de Sucesso (Status 200):
+Retorna um JSON contendo o preço total da reserva se o quarto estiver disponível para o período solicitado.
+{
+  "total_price": 840
+}
--- spec/requests/apis/booking_api_spec.rb
+++ spec/requests/apis/booking_api_spec.rb
@@ -1,107 +1,109 @@
 require 'rails_helper'
 
-context 'POST /api/v1/rooms/1/bookings/check_availability' do
-  it 'consulta disponibilidade de um quarto' do
-    #Arrange
-    paulo = User.create!(name: 'Paulo Menezes', email: 'paulomenezes@gmail.com', password: 'password', role: 'host')
-    guesthouse = Guesthouse.create!(corporate_name: 'Pousada Muro Alto Ltda', brand_name: 'Pousada Muro Alto', 
-                                    registration_number:'39165040000129', phone_number: '8134658799', email: 'pousadamuroalto@gmail.com', 
-                                    address: 'Av. Beira Mar, 45', neighborhood: 'Muro Alto', state: 'Pernambuco', city: 'Ipojuca', 
-                                    postal_code: '54350820', description: 'Pousada a beira mar maravilhosa', 
-                                    payment_method: 'Dinheiro, pix e cartão', pet_agreement: 'sim',
-                                    usage_policy: 'Proibido fumar nas áreas de convivência', check_in: '13:00', check_out: '12:00',
-                                    status: 'active', user: paulo)
-    room = Room.create!(name: 'Quarto Girassol', description: 'Quarto amplo com vista para o mar', area: '10', max_guest: '4', 
-                        default_price: '210,00', bathroom: 'sim', balcony: 'sim', air_conditioner: 'sim', tv: 'sim', wardrobe: 'sim', 
-                        safe: 'não', accessible: 'sim', status: 'available', guesthouse: guesthouse)
-    booking_params = { booking: {check_in_date: "#{1.day.from_now}", check_out_date: "#{5.days.from_now}", guests_number: '2', room: room}}
+describe 'Booking API' do
+  context 'POST /api/v1/rooms/1/bookings/check_availability' do
+    it 'consulta disponibilidade de um quarto' do
+      #Arrange
+      paulo = User.create!(name: 'Paulo Menezes', email: 'paulomenezes@gmail.com', password: 'password', role: 'host')
+      guesthouse = Guesthouse.create!(corporate_name: 'Pousada Muro Alto Ltda', brand_name: 'Pousada Muro Alto', 
+                                      registration_number:'39165040000129', phone_number: '8134658799', email: 'pousadamuroalto@gmail.com', 
+                                      address: 'Av. Beira Mar, 45', neighborhood: 'Muro Alto', state: 'Pernambuco', city: 'Ipojuca', 
+                                      postal_code: '54350820', description: 'Pousada a beira mar maravilhosa', 
+                                      payment_method: 'Dinheiro, pix e cartão', pet_agreement: 'sim',
+                                      usage_policy: 'Proibido fumar nas áreas de convivência', check_in: '13:00', check_out: '12:00',
+                                      status: 'active', user: paulo)
+      room = Room.create!(name: 'Quarto Girassol', description: 'Quarto amplo com vista para o mar', area: '10', max_guest: '4', 
+                          default_price: '210,00', bathroom: 'sim', balcony: 'sim', air_conditioner: 'sim', tv: 'sim', wardrobe: 'sim', 
+                          safe: 'não', accessible: 'sim', status: 'available', guesthouse: guesthouse)
+      booking_params = { booking: {check_in_date: "#{1.day.from_now}", check_out_date: "#{5.days.from_now}", guests_number: '2', room: room}}
 
-    #Act
-    post "/api/v1/rooms/#{room.id}/bookings/check_availability", params: booking_params
+      #Act
+      post "/api/v1/rooms/#{room.id}/bookings/check_availability", params: booking_params
 
-    #Assert
-    expect(response.status).to eq 200
-    expect(response.content_type).to include 'application/json'
-    json_response = JSON.parse(response.body)
-    expect(json_response["total_price"]).to eq 840
-  end
+      #Assert
+      expect(response.status).to eq 200
+      expect(response.content_type).to include 'application/json'
+      json_response = JSON.parse(response.body)
+      expect(json_response["total_price"]).to eq 840
+    end
 
-  it 'falha se parametros não estão completos' do
-    #Arrange
-    paulo = User.create!(name: 'Paulo Menezes', email: 'paulomenezes@gmail.com', password: 'password', role: 'host')
-    guesthouse = Guesthouse.create!(corporate_name: 'Pousada Muro Alto Ltda', brand_name: 'Pousada Muro Alto', 
-                                    registration_number:'39165040000129', phone_number: '8134658799', email: 'pousadamuroalto@gmail.com', 
-                                    address: 'Av. Beira Mar, 45', neighborhood: 'Muro Alto', state: 'Pernambuco', city: 'Ipojuca', 
-                                    postal_code: '54350820', description: 'Pousada a beira mar maravilhosa', 
-                                    payment_method: 'Dinheiro, pix e cartão', pet_agreement: 'sim',
-                                    usage_policy: 'Proibido fumar nas áreas de convivência', check_in: '13:00', check_out: '12:00',
-                                    status: 'active', user: paulo)
-    room = Room.create!(name: 'Quarto Girassol', description: 'Quarto amplo com vista para o mar', area: '10', max_guest: '4', 
-                        default_price: '210,00', bathroom: 'sim', balcony: 'sim', air_conditioner: 'sim', tv: 'sim', wardrobe: 'sim', 
-                        safe: 'não', accessible: 'sim', status: 'available', guesthouse: guesthouse)
-    booking_params = { booking: {check_in_date: '', check_out_date: '', guests_number: '', room: room}}
+    it 'falha se parametros não estão completos' do
+      #Arrange
+      paulo = User.create!(name: 'Paulo Menezes', email: 'paulomenezes@gmail.com', password: 'password', role: 'host')
+      guesthouse = Guesthouse.create!(corporate_name: 'Pousada Muro Alto Ltda', brand_name: 'Pousada Muro Alto', 
+                                      registration_number:'39165040000129', phone_number: '8134658799', email: 'pousadamuroalto@gmail.com', 
+                                      address: 'Av. Beira Mar, 45', neighborhood: 'Muro Alto', state: 'Pernambuco', city: 'Ipojuca', 
+                                      postal_code: '54350820', description: 'Pousada a beira mar maravilhosa', 
+                                      payment_method: 'Dinheiro, pix e cartão', pet_agreement: 'sim',
+                                      usage_policy: 'Proibido fumar nas áreas de convivência', check_in: '13:00', check_out: '12:00',
+                                      status: 'active', user: paulo)
+      room = Room.create!(name: 'Quarto Girassol', description: 'Quarto amplo com vista para o mar', area: '10', max_guest: '4', 
+                          default_price: '210,00', bathroom: 'sim', balcony: 'sim', air_conditioner: 'sim', tv: 'sim', wardrobe: 'sim', 
+                          safe: 'não', accessible: 'sim', status: 'available', guesthouse: guesthouse)
+      booking_params = { booking: {check_in_date: '', check_out_date: '', guests_number: '', room: room}}
 
-    #Act
-    post "/api/v1/rooms/#{room.id}/bookings/check_availability", params: booking_params
+      #Act
+      post "/api/v1/rooms/#{room.id}/bookings/check_availability", params: booking_params
 
-    #Assert
-    expect(response.status).to eq 412
-    expect(response.content_type).to include 'application/json'
-    expect(response.body).to include 'Data de entrada não pode ficar em branco'
-    expect(response.body).to include 'Data de saída não pode ficar em branco'
-    expect(response.body).to include 'Quantidade de hóspedes não pode ficar em branco'
-  end
+      #Assert
+      expect(response.status).to eq 412
+      expect(response.content_type).to include 'application/json'
+      expect(response.body).to include 'Data de entrada não pode ficar em branco'
+      expect(response.body).to include 'Data de saída não pode ficar em branco'
+      expect(response.body).to include 'Quantidade de hóspedes não pode ficar em branco'
+    end
 
-  it 'falha se quarto não está disponível' do
-    #Arrange
-    paulo = User.create!(name: 'Paulo Menezes', email: 'paulomenezes@gmail.com', password: 'password', role: 'host')
-    guesthouse = Guesthouse.create!(corporate_name: 'Pousada Muro Alto Ltda', brand_name: 'Pousada Muro Alto', 
-                                    registration_number:'39165040000129', phone_number: '8134658799', email: 'pousadamuroalto@gmail.com', 
-                                    address: 'Av. Beira Mar, 45', neighborhood: 'Muro Alto', state: 'Pernambuco', city: 'Ipojuca', 
-                                    postal_code: '54350820', description: 'Pousada a beira mar maravilhosa', 
-                                    payment_method: 'Dinheiro, pix e cartão', pet_agreement: 'sim',
-                                    usage_policy: 'Proibido fumar nas áreas de convivência', check_in: '13:00', check_out: '12:00',
-                                    status: 'active', user: paulo)
-    room = Room.create!(name: 'Quarto Girassol', description: 'Quarto amplo com vista para o mar', area: '10', max_guest: '4', 
-                        default_price: '210,00', bathroom: 'sim', balcony: 'sim', air_conditioner: 'sim', tv: 'sim', wardrobe: 'sim', 
-                        safe: 'não', accessible: 'sim', status: 'available', guesthouse: guesthouse)
+    it 'falha se quarto não está disponível' do
+      #Arrange
+      paulo = User.create!(name: 'Paulo Menezes', email: 'paulomenezes@gmail.com', password: 'password', role: 'host')
+      guesthouse = Guesthouse.create!(corporate_name: 'Pousada Muro Alto Ltda', brand_name: 'Pousada Muro Alto', 
+                                      registration_number:'39165040000129', phone_number: '8134658799', email: 'pousadamuroalto@gmail.com', 
+                                      address: 'Av. Beira Mar, 45', neighborhood: 'Muro Alto', state: 'Pernambuco', city: 'Ipojuca', 
+                                      postal_code: '54350820', description: 'Pousada a beira mar maravilhosa', 
+                                      payment_method: 'Dinheiro, pix e cartão', pet_agreement: 'sim',
+                                      usage_policy: 'Proibido fumar nas áreas de convivência', check_in: '13:00', check_out: '12:00',
+                                      status: 'active', user: paulo)
+      room = Room.create!(name: 'Quarto Girassol', description: 'Quarto amplo com vista para o mar', area: '10', max_guest: '4', 
+                          default_price: '210,00', bathroom: 'sim', balcony: 'sim', air_conditioner: 'sim', tv: 'sim', wardrobe: 'sim', 
+                          safe: 'não', accessible: 'sim', status: 'available', guesthouse: guesthouse)
 
-    guest = User.create!(name: 'João da Silva', email: 'joao@gmail.com', password: 'password', role: 'guest')
-    booking = Booking.create!(check_in_date: "#{1.week.from_now}", check_out_date: "#{2.weeks.from_now}", guests_number: '2', 
-                              room: room, user: guest)
+      guest = User.create!(name: 'João da Silva', email: 'joao@gmail.com', password: 'password', role: 'guest')
+      booking = Booking.create!(check_in_date: "#{1.week.from_now}", check_out_date: "#{2.weeks.from_now}", guests_number: '2', 
+                                room: room, user: guest)
 
-    booking_params = { booking: {check_in_date: "#{1.week.from_now}", check_out_date: "#{2.weeks.from_now}", guests_number: '2', room: room}}
+      booking_params = { booking: {check_in_date: "#{1.week.from_now}", check_out_date: "#{2.weeks.from_now}", guests_number: '2', room: room}}
 
-    #Act
-    post "/api/v1/rooms/#{room.id}/bookings/check_availability", params: booking_params
+      #Act
+      post "/api/v1/rooms/#{room.id}/bookings/check_availability", params: booking_params
 
-    #Assert
-    expect(response.status).to eq 412
-    expect(response.content_type).to include 'application/json'
-    expect(response.body).to include 'O quarto não está disponível para o período selecionado.'
-  end
+      #Assert
+      expect(response.status).to eq 412
+      expect(response.content_type).to include 'application/json'
+      expect(response.body).to include 'O quarto não está disponível para o período selecionado.'
+    end
 
-  it 'falha se tiver um erro interno' do
-    #Arrange
-    paulo = User.create!(name: 'Paulo Menezes', email: 'paulomenezes@gmail.com', password: 'password', role: 'host')
-    guesthouse = Guesthouse.create!(corporate_name: 'Pousada Muro Alto Ltda', brand_name: 'Pousada Muro Alto', 
-                                    registration_number:'39165040000129', phone_number: '8134658799', email: 'pousadamuroalto@gmail.com', 
-                                    address: 'Av. Beira Mar, 45', neighborhood: 'Muro Alto', state: 'Pernambuco', city: 'Ipojuca', 
-                                    postal_code: '54350820', description: 'Pousada a beira mar maravilhosa', 
-                                    payment_method: 'Dinheiro, pix e cartão', pet_agreement: 'sim',
-                                    usage_policy: 'Proibido fumar nas áreas de convivência', check_in: '13:00', check_out: '12:00',
-                                    status: 'active', user: paulo)
-    room = Room.create!(name: 'Quarto Girassol', description: 'Quarto amplo com vista para o mar', area: '10', max_guest: '4', 
-                        default_price: '210,00', bathroom: 'sim', balcony: 'sim', air_conditioner: 'sim', tv: 'sim', wardrobe: 'sim', 
-                        safe: 'não', accessible: 'sim', status: 'available', guesthouse: guesthouse)
+    it 'falha se tiver um erro interno' do
+      #Arrange
+      paulo = User.create!(name: 'Paulo Menezes', email: 'paulomenezes@gmail.com', password: 'password', role: 'host')
+      guesthouse = Guesthouse.create!(corporate_name: 'Pousada Muro Alto Ltda', brand_name: 'Pousada Muro Alto', 
+                                      registration_number:'39165040000129', phone_number: '8134658799', email: 'pousadamuroalto@gmail.com', 
+                                      address: 'Av. Beira Mar, 45', neighborhood: 'Muro Alto', state: 'Pernambuco', city: 'Ipojuca', 
+                                      postal_code: '54350820', description: 'Pousada a beira mar maravilhosa', 
+                                      payment_method: 'Dinheiro, pix e cartão', pet_agreement: 'sim',
+                                      usage_policy: 'Proibido fumar nas áreas de convivência', check_in: '13:00', check_out: '12:00',
+                                      status: 'active', user: paulo)
+      room = Room.create!(name: 'Quarto Girassol', description: 'Quarto amplo com vista para o mar', area: '10', max_guest: '4', 
+                          default_price: '210,00', bathroom: 'sim', balcony: 'sim', air_conditioner: 'sim', tv: 'sim', wardrobe: 'sim', 
+                          safe: 'não', accessible: 'sim', status: 'available', guesthouse: guesthouse)
 
-    booking_params = { booking: {check_in_date: "#{1.week.from_now}", check_out_date: "#{2.weeks.from_now}", guests_number: '2', room: room}}
-    allow(Booking).to receive(:new).and_raise(ActiveRecord::ActiveRecordError) 
+      booking_params = { booking: {check_in_date: "#{1.week.from_now}", check_out_date: "#{2.weeks.from_now}", guests_number: '2', room: room}}
+      allow(Booking).to receive(:new).and_raise(ActiveRecord::ActiveRecordError) 
 
-    #Act
-    post "/api/v1/rooms/#{room.id}/bookings/check_availability", params: booking_params
+      #Act
+      post "/api/v1/rooms/#{room.id}/bookings/check_availability", params: booking_params
 
-    #Assert
-    expect(response.status).to eq 500
+      #Assert
+      expect(response.status).to eq 500
+    end
   end
 end
